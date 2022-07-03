717fee6505588efc49651aba6035356ad900303c
Darlan Araujo <darlanparaujo@outlook.com>
2022-07-03 17:40:27 -0300

    Desafio #2 - Atualização 4
    Responsividade - Falta configurar o menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const dropdown = document.querySelectorAll('.dropdown');
 
+
 dropdown.forEach((item) => {
 
-    item.addEventListener('click', () => {
+    item.addEventListener('click', (event) => {
         item.nextElementSibling.classList.toggle('active');
+        event.target.classList.toggle('active');
     });
 });
