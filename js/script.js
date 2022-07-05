74a7155dffecbe0b6c8c997e25f751cd1bb13e85
Darlan Araujo <darlanparaujo@outlook.com>
2022-07-05 17:39:08 -0300

    Desafio #2 - Atualização 6
    Parte final da responsividade. Falta alguns ajustes.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,15 @@ dropdown.forEach((item) => {
         event.target.classList.toggle('active');
     });
 });
+
+const menuMobile = document.querySelectorAll('.menu-mobile');
+const page = document.querySelector('.page');
+const wrapperMobile = document.querySelector('.wrapper-menu-mobile');
+
+menuMobile.forEach((item) => {
+    item.addEventListener('click', () => {
+        page.classList.toggle('active');
+    wrapperMobile.classList.toggle('active');
+    });
+});
+
